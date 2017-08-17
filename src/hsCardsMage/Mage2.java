788de6911446621f5a66e81334235e71e3d71140
Lukasz <ljablona@gmail.com>
2017-08-17 13:43:24 +0200

    CommmitHSCard

---
+++ src/hsCardsMage/Mage2.java
@@ -0,0 +1,15 @@
+package hsCardsMage;
+
+import hsMain.CardMage;
+
+public class Mage2 extends CardMage {
+	public Mage2 (String n,int m, int a, int l){
+		super(n,m,a,l);
+		super.name="Mage2";
+	}
+	
+	public Mage2(){
+		super("Mage2",1,5,4);
+		
+	}
+}
